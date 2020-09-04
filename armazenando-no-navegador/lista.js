c402286873fb352773bd516ac5d27a1ff2cee7e6
Guilherme <guilherme-allves@hotmail.com>
2020-09-04 00:08:27 -0300

    Modificado arquivo lista.html e style.html. Adicionado arquivo lista.js

---
+++ armazenando-no-navegador/lista.js
@@ -0,0 +1,14 @@
+//Criar uma variável com  os links das imagens no localStorage. (Variável)
+  //Testar se existe imagens, se não mostrar uma informação.
+
+//Criar o elemento pai da lista? ou faço isso no HTML. (Função)
+//Criar os elementos (linhas) da lista. (Função)
+  //Inserir imagem. 
+  //Inserir botão de remover.
+
+//Verifcar se o botão de remover de alguma imagem foi clicado.  (Evento) 
+  //Apagar o link da imagem do localStorage.  (Função)
+//Verificar se o botão remover todos foi clicado. (Evento)
+  //Apagar o localStorage "Favoritesr". (Função)
+
+//Atualizar a variável dos links. (Função)
