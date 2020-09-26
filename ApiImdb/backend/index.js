1d5b113675cba7dd992e9eec226958b8c1c3a43d
Guilherme Alves <guilherme-allves@hotmail.com>
2020-09-26 16:53:40 -0300

    ApiImdb - pesquisa funcinando

--- ApiImdb/backend/index.js
+++ ApiImdb/backend/index.js
@@ -2,42 +2,62 @@ const puppeteer = require('puppeteer')
 
 
 // Nome do filme que será pesquisado
-const movieName = jumanji
+const movieName = 'jumanji'
+
 
 // função que pesquisa o filme
 async function searchMovie() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto(`https://www.imdb.com/find?q=${movieName}`)
-
+  // await page.goto('https://www.imdb.com/title/tt2283362/?ref_=fn_al_tt_1')
+ 
   
   // Validar a página buscando o valor 'Title'
   const title = await page.evaluate(() => {
     
-    //VALIDAÇÂO...
-
-    // return title
+    //VALIDAÇÃO...
+    title = document.querySelector('.originalTitle')
+    
+        
+    
   })
+  
+  
+  console.log('Fechou!')
+  await browser.close()
+  // return title
+}
 
-  // Testar se está na página dos filmes relacionados a pesquisa
-  if (title) {
+
+function verification(title) {
+  // Testar se está na página dos filmes relacionados a pesquisa (lista)
+  if (title == null) {
     createPageList()
   } 
   else {
     createPageMovie()
   }
-
-  await browser.close()
 }
 
 
 
+searchMovie()
+verification()
+
+
+
+
+
 // Criar a página dos filmes relacionados a pesquisa
-function createPageList() {}
+function createPageList() {
+  console.log('OK')
+ 
+}
 
 // Criar a página do filme pesquisado
-function createPageMovie() {}
+function createPageMovie() {
+  console.log('NotOK')
+}
 
 
-
-searchMovie()
