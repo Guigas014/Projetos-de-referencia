3a4745d9bbfb0c95cabd3730149832b542aaeb77
Guilherme Alves <guilherme-allves@hotmail.com>
2020-09-27 01:22:29 -0300

    ApiImdb - list.js 92%

--- ApiImdb/backend/list.js
+++ ApiImdb/backend/list.js
@@ -12,25 +12,17 @@ module.exports = {
         // Validar a página buscando o valor 'Title'
         const movieList = await page.evaluate(() => {
         
-            // const namesList = String(document.querySelector('.findSection').innerText).replace(/\t/g, "")
-            // const arrayList = namesList.split('\n')
-            // arrayList.shift(), arrayList.pop()
-            
-            // const linksList = String(document.querySelector('.findSection').innerHTML).replace(/\t/g, "")
-            // const linksList = String(document.getElementsByClassName('result_text')[0].innerText)
-            const linksList = document.querySelectorAll('.result_text a')
-
             // const linksList = String(document.getElementsByClassName('result_text')[0].firstElementChild.innerHTML)
             // const linksList = String(document.getElementsByClassName('result_text')[0].firstElementChild.href)
-            // const listlength = document.getElementsByClassName('result_text').length
-            
-            // const nodeList = document.querySelector('.findSection').innerHTML
+            // const linksList = document.querySelectorAll('.result_text a')
+            const linksList = document.querySelectorAll('.result_text')
             const dataArray = [ ...linksList ]
-            const linkList = dataArray.map( ({ href  }) => ( href ))
-            const nameList = dataArray.map( ({ innerHTML }) => ( innerHTML ))
-        
 
-            return nameList
+            const linkList = dataArray.map( ({ innerText }) => ( innerText ))
+            // const linkList = dataArray.map( ({ href }) => ( href ))
+            // const nameList = dataArray.map( ({ innerHTML }) => ( innerHTML ))
+            
+            return linkList
         });
         
 
