f7096a010c994694c64e9551abb01e37362183e8
Guilherme Alves <guilherme-allves@hotmail.com>
2020-09-28 14:26:40 -0300

    ApiImdb - list.js 99% (falta retorno)

--- ApiImdb/backend/list.js
+++ ApiImdb/backend/list.js
@@ -12,26 +12,35 @@ module.exports = {
         // Validar a página buscando o valor 'Title'
         const movieList = await page.evaluate(() => {
         
+            //Exemplos
             // const linksList = String(document.getElementsByClassName('result_text')[0].firstElementChild.innerHTML)
             // const linksList = String(document.getElementsByClassName('result_text')[0].firstElementChild.href)
-            // const linksList = document.querySelectorAll('.result_text a')
-            const linksList = document.querySelectorAll('.result_text')
-            const dataArray = [ ...linksList ]
-
-            const linkList = dataArray.map( ({ innerText }) => ( innerText ))
-            // const linkList = dataArray.map( ({ href }) => ( href ))
-            // const nameList = dataArray.map( ({ innerHTML }) => ( innerHTML ))
             
-            return linkList
+            // Busca os nomes dos filmes
+            // const names = document.querySelectorAll('.result_text')
+            // const nameArray = [ ...names ]
+            // const nameList = nameArray.map( ({ innerText }) => ( innerText ))
+            
+            //Busca os links e nomes dos filmes listados
+            const data = document.querySelectorAll('.result_text a')
+            const dataArray = [ ...data ]
+            const linkList = dataArray.map( ({ href }) => ( href ))
+            const nameList = dataArray.map( ({ innerText }) => ( innerText ))
+            
+            
+            // const length = nameArray.length
+            
+            
+            return nameList
         });
         
 
         console.log('Fechou01!')
         await browser.close()
-    
-        // return title
         
         console.log(movieList)
+        
+        // return title
         }
 }
 
