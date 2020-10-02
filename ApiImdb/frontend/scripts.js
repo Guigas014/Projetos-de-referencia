199c1a1c85be98f721956c20d1bd619944cbe785
Guilherme <guilherme-allves@hotmail.com>
2020-10-02 00:35:01 -0300

    ApiImdb - adicinado arquivos /backend/package.json e server.js | modificado arquivo scripts.js

---
+++ ApiImdb/frontend/scripts.js
@@ -0,0 +1,14 @@
+// Pega o filme pesquisado.
+document.getElementById('searchButton')
+  .onclick = function getName() {
+
+    const name = document.querySelector('input').value
+  
+    //console.log(name)
+    return name
+}
+
+
+
+//Enviar para o backend
+
