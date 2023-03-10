dd8cfd4d7c5d2c1fb3e6b8c90d6beba541ff7552
Guilherme Alves Araujo <guilherme-allves@hotmail.com>
2023-03-10 19:31:20 -0300

    authlocal - finalizado

--- mobile/authlocal/App.tsx
+++ mobile/authlocal/App.tsx
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react'; 
+import { Text, View, Button, Alert } from 'react-native';
+import * as LocalAuthentication from 'expo-local-authentication';
+
+import { styles } from './styles';
+
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  //Verifica se o aparelho é compativel com a autenticação.
+  async function verifyAvailableAuthentication() {
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+    console.log(compatible);
+
+    //Mostra quais as opções de autenticação.
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    console.log(types.map(type => LocalAuthentication.AuthenticationType[type]));
+  }
+
+  async function handleAuthentication() {
+    //Verifica se existe uma biometria cadastrada no dispositivo.
+    const isBiometricEnrolled = await LocalAuthentication.isEnrolledAsync();
+    //console.log(isBiometricEnrolled);
+    
+    if (!isBiometricEnrolled) {
+      return Alert.alert(
+        'Login', 
+        'Nenhuma biometria encontrada. Por favor, cadastre uma no dispositivo!'
+      )
+    }
+
+    //Inicia a autenticação
+    const auth = await LocalAuthentication.authenticateAsync({
+      promptMessage: 'Login com Biometria',
+      fallbackLabel: 'Biometria não reconhecida'
+    });
+
+    //console.log(auth);
+    setIsAuthenticated(auth.success);
+  }  
+
+  useEffect(() => {
+    verifyAvailableAuthentication(); 
+  }, []);
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Usuário conectado: { 
+          isAuthenticated ? 
+            <Text style={{ color: "green", fontSize: 18 }}>Sim</Text>
+          : 
+            <Text style={{ color: "red", fontSize: 18 }}>Não</Text>
+          }
+      </Text>
+
+      <Button
+        title="Entrar"
+        onPress={handleAuthentication} 
+      />      
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/authlocal/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 20,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  confirmation: {
+    fontSize: 18,
+    color: "blue",
+  },
+});
